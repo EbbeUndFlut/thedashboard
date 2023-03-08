2efbcc69131a3d9ac0ff8f4a3b153a58ef409abc
Christian Peters <c.peters@makermaker.de>
2023-03-08 12:18:38 +0100

    setup first page

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,7 +1,8 @@
 import test, { expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
+test('is the right page', async ({ page }) => {
     await page.goto('http://localhost:5173')
-
-    await expect(page).toHaveTitle(/Vite/)
+    await page.screenshot({ path: 'screenshot.png', fullPage: true });
+    const locator = page.locator('h1') // get the <h1> element
+    await expect(locator).toContainText('Login') // check if the <h1> element contains the string 'Login"
 })
---
+++ src/pages/Login.tsx
@@ -0,0 +1,10 @@
+const Login = () => {
+    console.log(import.meta.env.VITE_FRONTEND_URL)
+    return (
+        <main>
+            <h1>Login</h1>
+        </main>
+    )
+}
+
+export default Login
